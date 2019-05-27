1627da7397a47fde9ad621deaef01305cc33d637
JinhaSong <thejn07@gmail.com>
2019-05-28 04:29:29 +0900

    Add function to profile execution time of function

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
+import timeit
 
 def load_binary_image(image_path) :
     b_image = open(image_path, 'rb')
     return b_image
+
+def profile_function_time(function) :
+    start = timeit.default_timer()
+    response = function
+    end = timeit.default_timer()
+
+    time = end-start
+    return response, time
