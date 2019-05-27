eebde1fd3ad10cf79d7fa2b2b36c7a15f16a644b
JinhaSong <thejn07@gmail.com>
2019-05-28 04:33:20 +0900

    Modify main file - Rename pars_args -> parse_arguments - Add parse_arguements calling syntaxes - Add source code to send a request message and return the response time and response

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from AnalysisSiteRequestGenerator import AnalysisSiteRequestGenerator
 from utils import *
 import argparse
 
-def parse_args():
+def parse_arguments():
     """Parse input arguments"""
     parser = argparse.ArgumentParser(description='sogang-mm analysis-site requests generator')
     parser.add_argument("--url", dest='url', help='URL of analysis-site', type=str)
-    parser.add_argument("--image_path", dest='image_path', help='Image path to send to request', type=str)
+    parser.add_argument("--image_path", dest='image_path', help='Image path to send as request', type=str)
     parser.add_argument("--modules", dest='modules', help='names of analysis-module', type=str)
-    parser.add_argument("--numofmessages", dest='num_of_messages', help="number of request", type=int)
+    parser.add_argument("--numofmessages", dest='num_of_messages', help="number of request messages to send", type=int)
 
-    args = parser.parse_args()
-    return args
+    return parser
 
 if __name__ == '__main__':
-    pass
+    try :
+        args = parse_arguments().parse_args()
+
+        b_image = load_binary_image(args.image_path)
+        request_generator = AnalysisSiteRequestGenerator()
+        request_generator.set_request_attr(url=args.url, image=b_image, modules=None)
+        num_of_messages = args.num_of_messages
+
+        for i in range(0, num_of_messages) :
+            response, time = profile_function_time(request_generator.send_request_message())
+
+    except :
+        parse_arguments().print_help()
