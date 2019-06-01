4beb7f21ad893d658fda7d8a5d66bd9ef953d59b
JinhaSong <thejn07@gmail.com>
2019-06-01 15:51:03 +0900

    Fixe an error that a function can not correctly measure time.

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
-import timeit
+import time
 
 def load_binary_image(image_path) :
     b_image = open(image_path, 'rb')
     return b_image
 
-def profile_function_time(function) :
-    start = timeit.default_timer()
-    response = function
-    end = timeit.default_timer()
-
-    time = end-start
-    return response, time
+def profile_function_time(function):
+    def wrap(*args):
+        time1 = time.time()
+        response = function(*args)
+        time2 = time.time()
+        print('%s:\t%0.3f' % (function.func_name, time2-time1))
+        return response
+    return wrap
